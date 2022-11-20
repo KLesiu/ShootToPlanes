ee9fd9242cb640033d076dd14d5bcd4013b0f87a
KLesiu <kubaxl222@gmail.com>
2022-11-20 20:52:40 +0100

    Add all levels. Add losing text on section when you lose level. Change animation time at style.css because I wanted to test game without tryharding.

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const next = document.querySelector(".next__panel");
 const plane = document.querySelector("#PlaneToShoot");
 const buttonStart = document.querySelector(".button__start");
 const miss = document.querySelector(".miss");
+const score = document.querySelector(".your__score");
+let showScore = document.querySelector(".showScore");
+let text = document.querySelector(".rules");
 let yourscore = 0;
 const startTheGame = () => {
   buttonStart.addEventListener("click", () => {
@@ -22,13 +25,11 @@ const nextLevel = () => {
   next.classList.remove("hidden");
   game.classList.add("hidden");
   if ((yourscore = 1)) {
-    const wynik = document.querySelector(".wynik");
-    wynik.innerText = "Wygrałeś!";
+    showScore.innerText = "Wygrałeś siema";
   }
 };
 const scorePlayer = () => {
   if (plane.classList.contains("active")) {
-    const score = document.querySelector(".your__score");
     yourscore = 1;
     score.innerHTML = yourscore;
     setTimeout(nextLevel, 1);
@@ -42,20 +43,55 @@ const missTarget = () => {
 const backToStart = () => {
   game.classList.add("hidden");
   start.classList.remove("hidden");
+  text.innerText = "Spróbuj jeszcze raz";
 };
-const mediumLevel = () => {
+// const mediumLevel = () => {
+//   const button = document.querySelector(".button__next");
+//   button.addEventListener("click", () => {
+//     plane.classList.remove("animationEasy");
+//     plane.classList.add("animationMedium");
+//     setTimeout(levelStart, 1000);
+//   });
+// };
+const switchLevel = () => {
   const button = document.querySelector(".button__next");
   button.addEventListener("click", () => {
-    plane.classList.remove("animationEasy");
-    plane.classList.add("animationMedium");
-    setTimeout(mediumLevelStart, 1000);
+    if (plane.classList.contains("animationEasy")) {
+      plane.classList.remove("animationEasy");
+      plane.classList.add("animationMedium");
+      setTimeout(levelStart, 1000);
+    } else if (plane.classList.contains("animationMedium")) {
+      plane.classList.remove("animationMedium");
+      plane.classList.add("animationHard");
+      yourscore = 2;
+      score.innerHTML = yourscore;
+      setTimeout(levelStart, 1000);
+    } else if (plane.classList.contains("animationHard")) {
+      plane.classList.remove("animationHard");
+      plane.classList.add("animationExpert");
+      setTimeout(levelStart, 1000);
+      yourscore = 3;
+      score.innerHTML = yourscore;
+    } else {
+    }
   });
 };
-const mediumLevelStart = () => {
+const levelStart = () => {
   game.classList.remove("hidden");
   next.classList.add("hidden");
 };
+// const hardLevel = () => {
+//   const button = document.querySelector(".button__next");
+//   button.addEventListener("click", () => {
+//     if (plane.classList.contains("animationMedium")) {
+//       plane.classList.remove("animationMedium");
+//       plane.classList.add("animationHard");
+//     }
+//   });
+// };
 startTheGame();
 planeStart();
 missTarget();
-mediumLevel();
+// mediumLevel();
+// hardLevel();
+switchLevel();
