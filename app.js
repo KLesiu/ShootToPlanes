fce0a382e8508b1c9766c42b902701719199a589
KLesiu <kubaxl222@gmail.com>
2022-11-21 17:41:05 +0100

    Repair function which resets score after click button. Optymalize game for smartphones and other mobile devices

--- app.js
+++ app.js
@@ -25,9 +25,11 @@ const nextLevel = () => {
   next.classList.remove("hidden");
   game.classList.add("hidden");
   if ((yourscore = 1)) {
-    showScore.innerText = "Wygrałeś";
+    showScore.innerText = "You won";
   }
 };
+const button = document.querySelector(".button__next");
+button.innerText = "Next level!";
 const scorePlayer = () => {
   if (plane.classList.contains("active")) {
     yourscore = 1;
@@ -43,7 +45,7 @@ const missTarget = () => {
 const backToStart = () => {
   game.classList.add("hidden");
   start.classList.remove("hidden");
-  text.innerText = "Spróbuj jeszcze raz";
+  text.innerText = "You lost.Try again";
 };
 // const mediumLevel = () => {
 //   const button = document.querySelector(".button__next");
@@ -78,13 +80,16 @@ const switchLevel = () => {
       next.classList.add("hidden");
       game.classList.add("hidden");
       start.classList.remove("hidden");
-      text.innerText = "Gratulację, ukończyłeś/aś wszystkie dostępne poziomy";
+      text.innerText =
+        "Congratulations, you completed all levels. If you want to play one more, click the button.";
       buttonReset();
       plane.classList.remove("animationExpert");
       plane.classList.remove("animationMedium");
       plane.classList.remove("animationHard");
       plane.classList.add("animationEasy");
       plane.classList.remove("active");
+      yourscore = 0;
+      score.innerHTML = yourscore;
     }
   });
 };
@@ -92,8 +97,6 @@ const buttonReset = () => {
   buttonStart.addEventListener("click", () => {
     if (plane.classList.contains("animationExpert")) {
       plane.classList.add("animationEasy");
-      yourscore = 0;
-      score.innerHTML = yourscore;
     }
   });
 };
@@ -105,10 +108,10 @@ const changeBackgroundMedium = () => {
   const body = document.querySelector("body");
   body.style.backgroundImage = "url(img/tlo.svg)";
 };
-const shoot = () => {
-  const audioTank = new Audio("mp3/tanksound.mp3");
-  audioTank.play();
-};
+// const shoot = () => {
+//   const audioTank = new Audio("mp3/tanksound.mp3");
+//   audioTank.play();
+// };
 
 // const hardLevel = () => {
 //   const button = document.querySelector(".button__next");
@@ -125,4 +128,4 @@ missTarget();
 // mediumLevel();
 // hardLevel();
 switchLevel();
-shoot();
+// shoot();
